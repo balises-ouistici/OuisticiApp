bfd0e61da2b37587d70423ea3b8af01912f3cf0f
Guillaume REY <guillaume.rey@etu.uca.fr>
2024-05-06 16:08:33 +0200

    push

--- application/app/src/main/java/com/example/ouistici/ui/choixAnnonce/ChoixAnnonceComposable.kt
+++ application/app/src/main/java/com/example/ouistici/ui/choixAnnonce/ChoixAnnonceComposable.kt
@@ -488,6 +488,8 @@ fun AddPlageHorairePopup(
                                         "Annonce ajoutée",
                                         Toast.LENGTH_LONG)
                                         .show()
+                                    onDismiss()
+                                    navController.navigate("manageAnnonce")
                                 }
                             } else {
                                 Toast.makeText(
@@ -496,8 +498,6 @@ fun AddPlageHorairePopup(
                                     Toast.LENGTH_LONG)
                                     .show()
                             }
-                            onDismiss()
-                            navController.navigate("manageAnnonce")
                         },
                         colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
                         modifier = Modifier.padding(end = 8.dp)
@@ -555,63 +555,6 @@ fun TimePickerDialog(
         Text(text = "Selected Time: ${mTime.value}", fontSize = 30.sp)
     }
 }
-
-
-/*
-@Composable
-fun TimePickerDialog(
-    title: String = "Select Time",
-    onDismissRequest: () -> Unit,
-    confirmButton: @Composable (() -> Unit),
-    dismissButton: @Composable (() -> Unit)? = null,
-    containerColor: Color = MaterialTheme.colorScheme.surface,
-    content: @Composable () -> Unit,
-) {
-    Dialog(
-        onDismissRequest = onDismissRequest,
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false
-        ),
-    ) {
-        Surface(
-            shape = MaterialTheme.shapes.extraLarge,
-            tonalElevation = 6.dp,
-            modifier = Modifier
-                .width(IntrinsicSize.Min)
-                .height(IntrinsicSize.Min)
-                .background(
-                    shape = MaterialTheme.shapes.extraLarge,
-                    color = containerColor
-                ),
-            color = containerColor
-        ) {
-            Column(
-                modifier = Modifier.padding(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 20.dp),
-                    text = title,
-                    style = MaterialTheme.typography.labelMedium
-                )
-                content()
-                Row(
-                    modifier = Modifier
-                        .height(40.dp)
-                        .fillMaxWidth()
-                ) {
-                    Spacer(modifier = Modifier.weight(1f))
-                    dismissButton?.invoke()
-                    confirmButton()
-                }
-            }
-        }
-    }
-}
-*/
-
 
 
 
