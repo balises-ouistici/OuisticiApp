40be60137b517f63ff15e7e415ac431e083ec8fb
Guillaume REY <guillaume.rey@etu.uca.fr>
2024-05-13 09:52:34 +0200

    selection de langue pour annonce de type texte possible

--- application/app/src/main/java/com/example/ouistici/ui/annonceTexte/AnnonceTexteComposable.kt
+++ application/app/src/main/java/com/example/ouistici/ui/annonceTexte/AnnonceTexteComposable.kt
@@ -1,19 +1,20 @@
 package com.example.ouistici.ui.annonceTexte
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -30,10 +31,12 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.ouistici.R
 import com.example.ouistici.model.Annonce
 import com.example.ouistici.model.Balise
+import com.example.ouistici.model.Langue
+import com.example.ouistici.model.LangueManager
 import com.example.ouistici.model.TypeAnnonce
+import com.example.ouistici.ui.parametresAppli.DropdownMenuItemLangue
 import com.example.ouistici.ui.theme.FontColor
 
 @Composable
@@ -42,6 +45,8 @@ fun AnnonceTexte(navController: NavController, balise: Balise) {
     var textContenu by remember { mutableStateOf(TextFieldValue()) }
     var textValueInput by remember { mutableStateOf("") }
     var textContenuInput by remember { mutableStateOf("") }
+    var langueSelectionnee by remember { mutableStateOf(Langue("", "")) }
+    var expanded by remember { mutableStateOf(false) }
 
     var context = LocalContext.current
 
@@ -90,10 +95,52 @@ fun AnnonceTexte(navController: NavController, balise: Balise) {
         Spacer(modifier = Modifier.height(20.dp))
 
         // FAIRE CHOIX DE LANGUE POUR SYNTHETISEUR
-        Text(
-            text = "Langue de la voix :",
-            color = FontColor
-        )
+        Row (verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "Langue de la voix :",
+                color = FontColor
+            )
+
+
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable(onClick = { expanded = true })
+            ) {
+
+                if ( langueSelectionnee.code == "" && langueSelectionnee.nom == "" ) {
+                    Text(
+                        text = "Choisir",
+                        fontSize = 16.sp,
+                        color = FontColor
+                    )
+                } else {
+                    Text(
+                        text = langueSelectionnee.getLangueName(),
+                        fontSize = 16.sp,
+                        color = FontColor
+                    )
+                }
+
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    LangueManager.languesDisponibles.forEach { langue ->
+                        DropdownMenuItemLangue(
+                            langue = langue,
+                            onClick = {
+                                langueSelectionnee = langue
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+
+        Text(text = langueSelectionnee.toString())
 
 
         Spacer(modifier = Modifier.height(50.dp))
@@ -101,8 +148,8 @@ fun AnnonceTexte(navController: NavController, balise: Balise) {
         
         Button(
             onClick = {
-                if ( textValueInput != "" && textContenuInput != "" ) {
-                    balise.annonces?.add(Annonce(textValueInput, TypeAnnonce.TEXTE, null, textContenuInput, null))
+                if ( textValueInput != "" && textContenuInput != "" && langueSelectionnee.code != "" && langueSelectionnee.nom != "" ) {
+                    balise.annonces.add(Annonce(textValueInput, TypeAnnonce.TEXTE, null, textContenuInput, langueSelectionnee))
                     Toast.makeText(
                         context,
                         "Annonce ajoutée",
@@ -128,4 +175,19 @@ fun AnnonceTexte(navController: NavController, balise: Balise) {
         }
 
     }
+}
+
+
+@Composable
+fun DropdownMenuItemLangue(
+    langue: Langue,
+    onClick: (Langue) -> Unit
+) {
+    Text(
+        text = langue.getLangueName(),
+        fontSize = 16.sp,
+        modifier = Modifier
+            .clickable(onClick = { onClick(langue) })
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+    )
 }
